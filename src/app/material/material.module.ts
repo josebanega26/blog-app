520beeddb3942cfc2667d57ac8c5b3716eba20f6
Jose Banega <banegajose26@gmail.com>
2020-06-02 22:05:06 -0300

    Add pagination &  Icon

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatCardModule } from "@angular/material/card";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatCardModule, MatExpansionModule],
-  exports: [MatCardModule, MatExpansionModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatPaginatorModule,
+  ],
+  exports: [MatCardModule, MatExpansionModule, MatPaginatorModule],
 })
 export class MaterialModule {}
--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { PostService } from "../post.service";
 import { Post } from "../post.interface";
 import { Subscription } from "rxjs";
+import { PageEvent } from "@angular/material/paginator";
+PageEvent;
 @Component({
   selector: "app-posts-list",
   templateUrl: "./posts-list.component.html",
   styleUrls: ["./posts-lists.component.scss"],
 })
 export class PostsListComponent implements OnInit, OnDestroy {
+  totalPost: number = 50;
+  postsPerPage: number = 5;
   postsList: Post[] = [];
   postsSubscription: Subscription;
   constructor(private postService: PostService) {}
@@ -25,5 +29,9 @@ export class PostsListComponent implements OnInit, OnDestroy {
   deletePost(id) {
     console.log("Delete id", id);
     this.postService.deletePost(id);
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log("pageData", pageData);
   }
 }

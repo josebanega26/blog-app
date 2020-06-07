640c149179d038a3d59eac4634d1f9ef0b6a14d7
Jose Banega <banegajose26@gmail.com>
2020-06-07 13:50:42 -0300

    Add basic structure for sigup page

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signUpForm: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.setForm();
   }
-
+  setForm() {
+    this.signUpForm = new FormGroup({
+      email: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
 }
